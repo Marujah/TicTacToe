27e8359266417e071f7936f6b755e1ccd4dfc008
Marujah <marujah@gmail.com>
2018-01-25 15:10:19 +0100

    add checks to bot game

--- components/Main.js
+++ components/Main.js
@@ -3,7 +3,8 @@ import { View, StyleSheet } from 'react-native';
 import Zell from './Zell';
 import Player from './Player';
 
-let playedZells = []
+let playedZells = [];
+let botPlayedZells = [];
 
 export default class Main extends Component {
   constructor(props){
@@ -64,50 +65,66 @@ export default class Main extends Component {
     });
     return result; 
   }
+  generateRandomIndex(length){  
+    return  Math.floor(Math.random()*length)
+  }
+  resetStates() {
+    playedZells = [];
+      botPlayedZells = [];
+      this.setState({player:[], bot:[], allZells:[
+        {index: 0, val:'', active: true},
+        {index: 1, val:'', active: true},
+        {index: 2, val:'', active: true},
+        {index: 3, val:'', active: true},
+        {index: 4, val:'', active: true},
+        {index: 5, val:'', active: true},
+        {index: 6, val:'', active: true},
+        {index: 7, val:'', active: true},
+        {index: 8, val:'', active: true}
+      ]});
+  }
+  isWinner(playedZells,player) {
+    if(playedZells.length>2) {
+      let allComb = this.getAllComb(playedZells);
+      if (allComb.some( (item) => { return this.getAllWinComb(this.props.winningCombinations).indexOf(item.join('')) !== -1; })) {
+        alert(player + ' win(s)');
+        return true;
+      }
+    }
+    return false;
+  }
   botPlays (playedZell) {
     playedZells.push(playedZell);
     let data = this.state.allZells.filter((item) => playedZells.indexOf(item.index) === -1);
-    let randomIndex = Math.floor(Math.random()*data.length);
+    let randomIndex = this.generateRandomIndex(data.length);
+    while (!data[randomIndex].active) {
+      randomIndex = this.generateRandomIndex(data.length);
+    }
     data[randomIndex].val = 'O';
     data[randomIndex].active = false;
     let newZells = this.state.allZells;
-    // newZells.find((i) => i.index === data)
-    // newZells[randomIndex].val='O';
-    // let freeBotZell = data.find((zell) => zell.index === randomIndex).val='O';
     this.setState({allZells:newZells});
+    botPlayedZells.push(randomIndex);
+    // check if bot wins
+    if (this.isWinner(botPlayedZells, 'Bot')) this.resetStates();
   }
   onCheckGameStatus (index) {
-    let winningCombinations = ['012', '036', '048','345','678','147','258','246'];
+    // let winningCombinations = ['012', '036', '048','345','678','147','258','246'];
     let data = this.state.player;
     data.push(index);
     let allZellsData = this.state.allZells;
     allZellsData[index].val='X';
     allZellsData[index].active=false;
     this.setState({player: data, allZells: allZellsData});
-    // this.botZells.push(index);   
-    if(this.state.player.length >= 3) {
-      // get all 3 combis from state player
-      let allComb = this.getAllComb(this.state.player);
-      // check if one of them is in allwinningComb
-      if (allComb.some( (item) => { return this.getAllWinComb(winningCombinations).indexOf(item.join('')) !== -1; })) {
-          // player wins
-          alert('Player wins');
-          playedZells = [];
-          this.setState({player:[], bot:[], allZells:[
-            {index: 0, val:'', active: true},
-            {index: 1, val:'', active: true},
-            {index: 2, val:'', active: true},
-            {index: 3, val:'', active: true},
-            {index: 4, val:'', active: true},
-            {index: 5, val:'', active: true},
-            {index: 6, val:'', active: true},
-            {index: 7, val:'', active: true},
-            {index: 8, val:'', active: true}
-          ]});
-      } else {
-        this.botPlays(index);
-      }
-    } else this.botPlays(index)
+
+    if (this.isWinner(this.state.player, 'Player')) {
+      this.resetStates();
+    } else {
+      this.botPlays(index);
+    }  
+    if (this.state.player.length === 5 && !this.isWinner(this.state.player, 'Player') && !this.isWinner(botPlayedZells, 'bot') ) {  
+      alert('Draw');this.resetStates();
+    }
   }
   render() {
     return (
